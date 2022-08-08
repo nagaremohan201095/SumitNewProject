600b17d859ed7a762c6f74044e202ace34631d26
nagaremohan201095 <nagaremohan201095@gmail.com>
2022-08-08 15:55:52 +0530

    SMS-01: File

---
+++ src/main/java/com/BikkadIT/model/Student.java
@@ -0,0 +1,52 @@
+package com.BikkadIT.model;
+
+public class Student {
+	
+	private int sid;
+	
+	private String sName;
+	
+	private String sEmail;
+	
+	private int sAge;
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getsName() {
+		return sName;
+	}
+
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+
+	public String getsEmail() {
+		return sEmail;
+	}
+
+	public void setsEmail(String sEmail) {
+		this.sEmail = sEmail;
+	}
+
+	public int getsAge() {
+		return sAge;
+	}
+
+	public void setsAge(int sAge) {
+		this.sAge = sAge;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sName=" + sName + ", sEmail=" + sEmail + ", sAge=" + sAge + "]";
+	}
+	
+	
+
+}
